3e1eb8a7bdc0aee7bf69683706858fc0289e6a64
heye <326524197@qq.com>
2019-12-22 18:38:14 +0800

    fix

--- springboot-producer/src/main/java/com/sxw/springbootproducer/ProducerApplication.java
+++ springboot-producer/src/main/java/com/sxw/springbootproducer/ProducerApplication.java
@@ -1,9 +1,11 @@
 package com.sxw.springbootproducer;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.sxw.springbootproducer.mapping")
 public class ProducerApplication {
 
     public static void main(String[] args) {
