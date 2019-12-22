9a5bcafcc3fe0a0b10e8e0e6ff7baa9819bc00fe
heye <326524197@qq.com>
2019-12-22 19:03:07 +0800

    fix

--- springboot-producer/src/test/java/com/sxw/springbootproducer/SpringbootProducerApplicationTests.java
+++ springboot-producer/src/test/java/com/sxw/springbootproducer/SpringbootProducerApplicationTests.java
@@ -8,19 +8,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
 import java.util.UUID;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringbootProducerApplicationTests {
-    @Autowired private OrderService orderService;
+    @Autowired
+    private OrderService orderService;
+
     @Test
     public void testSend() throws Exception {
         Order order = new Order();
-        order.setId(2018092101);
+        order.setId(Integer.parseInt(getOrderIdByTime()));
         order.setName("测试订单1");
-        order.setMessageId(System.currentTimeMillis()+"$"+UUID.randomUUID().toString());
+        order.setMessageId(System.currentTimeMillis() + "$" + UUID.randomUUID().toString());
         orderService.createOrder(order);
+    }
+
+    public static String getOrderIdByTime() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+        String newDate = sdf.format(new Date());
+        String result = "";
+        Random random = new Random();
+        for (int i = 0; i < 2; i++) {
+            result += random.nextInt(10);
+        }
+        return newDate + result;
     }
 
 }
